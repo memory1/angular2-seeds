36e5ff4dc7db7b19f234d8d39550c0cfa6e42fc5
memory1 <mshen_85@hotmail.com>
2019-03-03 21:34:50 +0800

    a

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,7 @@
 import {Component} from '@angular/core';
 import { Employee } from '../models/employee.model';
+import { FormPoster } from '../services/form-poster.service';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'home',
@@ -9,4 +11,20 @@ import { Employee } from '../models/employee.model';
 export class HomeComponent {
   languages = ["English","Spanish"];
   model = new Employee('Dala','Smith',true,"w2","spanish");
+  hasPrimaryLanguageError = false;
+
+  constructor(private formPoster: FormPoster){
+
+  }
+  validatePrimaryLanguage(value){
+    if (value === "default")
+      this.hasPrimaryLanguageError = true;
+    else
+      this.hasPrimaryLanguageError = false;
+    console.log("lang: " + value);
+  }
+
+  submitForm(form: NgForm){
+    this.formPoster.postEmployeeForm(this.model);
+  }
 }
